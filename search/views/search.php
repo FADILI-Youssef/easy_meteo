b851cc67534b8cf6206a4681c491465fce81d635
fadili.youssef <fadiliyous@yahoo.fr>
2015-09-30 18:22:07 +0200

    Syle recherche

--- search/views/search.php
+++ search/views/search.php
@@ -3,44 +3,53 @@
     <h2>RECHERCHE</h2> 
      
      <!-- Formulaire de recherche -->
-    <form methode="POST" action="">
-        <!-- Ville -->
-        <label for="ville">Ville </label>
-        <input type="text" id="ville" />
+            <!-- Ville -->
+            <span>
+                <label for="ville">Ville</label><input type="text" id="ville" />
+            
 
-        <!-- Type éolienne -->
-        <label for="type_eolienne">Type éolienne </label>
-        <select id="type_eolienne"></select>
+            <!-- Type éolienne -->
+            <span>
+                <label for="type_eolienne">Type éolienne</label><select id="type_eolienne"><option></option><option>Premier test</option><option>Second test</option></select>
+            </span>
 
-        <!-- Diamètre éolienne -->
-        <label for="diametre_eolienne">Diamètre éolienne </label>
-        <select id="diametre_eolienne"></select>
+            <!-- Diamètre éolienne -->
+             <span>   
+                <label for="diametre_eolienne">Diamètre éolienne</label><select id="diametre_eolienne"><option></option><option>Premier test</option><option>Second test</option></select>
+             </span>
 
-        <!-- Mois de début -->
-        <label for="mois_debut">Mois de début </label>
-        <select id="mois_debut"></select>
+            <!-- Mois de début -->
+            <span>
+                <label for="mois_debut">Mois de début</label><select id="mois_debut"><option></option><option>Premier test</option><option>Second test</option></select>
+            </span>
 
-        <!-- Mois de fin -->
-        <label for="mois_fin">Mois de fin </label>
-        <select id="mois_fin"></select>
+            <!-- Mois de fin -->
+            <span>
+                <label for="mois_fin">Mois de fin</label><select id="mois_fin"><option></option><option>Premier test</option><option>Second test</option></select>
+            </span>
 
-        <!-- Période -->
-        <label for="periode">Période </label>
-        <select id="periode"></select>
+            <!-- Période -->
+            <span>
+                <label for="periode">Période</label><select id="periode"><option></option><option>Premier test</option><option>Second test</option></select>
+            </span>
 
-        <!-- Seuil vitesse moyenne du vent -->
-        <label for="seuil_vitesse_moyenne_vent">Seuil vitesse moyenne du vent </label>
-        <input type="number" id="seuil_vitesse_moyenne_vent" min="0" max="250"/>
+            <!-- Seuil vitesse moyenne du vent -->
+            <span>
+                <label for="seuil_vitesse_moyenne_vent">Seuil vitesse moyenne du vent</label><input type="number" id="seuil_vitesse_moyenne_vent" min="0" max="250"/>
+            </span>
 
-        <!-- Seuil température -->
-        <label for="seuil_temperature">Seuil de température </label>
-        <input type="number" id="seuil_temperature" min="-30" max="50" />
+            <!-- Seuil température -->
+            <span>
+                <label for="seuil_temperature">Seuil de température</label><input type="number" id="seuil_temperature" min="-30" max="50" />
+            </span>
 
-        <!-- Seuil durée d'insolation -->
-        <label for="seuil_duree_insolation">Seuil durée d'insolation </label>
-        <input type="number" id="seuil_duree_insolation" min="0" max="24" />
+            <!-- Seuil durée d'insolation -->
+            <span>
+                <label for="seuil_duree_insolation">Seuil durée d'insolation</label><input type="number" id="seuil_duree_insolation" min="0" max="24" />
+            </span>
 
-        <!-- Bouton rechercher -->
-        <input type="submit" value="Rechercher" />
-     </form>
+            <!-- Bouton rechercher -->
+            
+                <input id="submit_to_periodic_display" type="submit" value="Rechercher" />
+     
 </section>
--- style/smooth_scroll.js
+++ style/smooth_scroll.js
@@ -1,8 +1,18 @@
     $(document).ready(function() {
 		$('.scrollTo').click( function() { // Au clic sur un élément
 			var page = $(this).attr('href'); // Page cible
-			var speed = 750; // Durée de l'animation (en ms)
-			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			$('html, body').animate( { scrollTop: $(page).offset().top }, 750 ); // Go
+			return false;
+		});
+	});
+
+
+
+    $(document).ready(function() {
+		$('#submit_to_periodic_display').click( function() { // Au clic sur un élément
+            var id = $(this).attr('id');
+			var page = id.substring(10, id.length); // Page cible
+			$('html, body').animate( { scrollTop: $('#'+page).offset().top }, 750 ); // Go
 			return false;
 		});
 	});
