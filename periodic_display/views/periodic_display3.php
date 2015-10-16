e1cbd213e3d4826bb1f1f516345c9859041843b4
fadili.youssef <fadiliyous@yahoo.fr>
2015-10-16 22:14:53 +0200

    carousel js

--- periodic_display/views/periodic_display3.php
+++ periodic_display/views/periodic_display3.php
@@ -1,7 +1,7 @@
 <section id="periodic_display">
     
     
-    <img id="c_prec_button" class="c_navigation" src="images/c_prev.png" height="50" width="50" onclick="c_prec();" />
+    <img id="c_prec_button" class="c_navigation" src="images/c_prev.png" height="50" width="50" onclick="c_prec(this);" />
     <table border="0">
         <caption id="titre_periodes"><strong>Janvier</strong></caption>
         <thead>
@@ -48,6 +48,6 @@
             <tr><td colspan="5">Les données exploitées sont tirées de <a href="http://www.prevision-meteo.ch/" target="_blank">http://www.prevision-meteo.ch/</a></td></tr>
         </tfoot>
     </table>
-    <img id="c_suiv_button" class="c_navigation" src="images/c_next.png" height="50" width="50" onclick="c_suiv();" />
+    <img id="c_suiv_button" class="c_navigation" src="images/c_next.png" height="50" width="50" onclick="c_suiv(this);" />
 
 </section> 
